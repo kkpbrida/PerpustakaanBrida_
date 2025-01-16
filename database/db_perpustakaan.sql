7d4b7343cc86324d0903fefc1c4b873c9ab6acde
Seynz <elverandirham212@gmail.com>
2025-01-16 14:21:50 +0800

    mengubah struktur database

--- database/db_perpustakaan.sql
+++ database/db_perpustakaan.sql
@@ -1,263 +1,222 @@
--- -- phpMyAdmin SQL Dump
--- -- version 5.2.1
--- -- https://www.phpmyadmin.net/
--- --
--- -- Host: 127.0.0.1
--- -- Waktu pembuatan: 15 Jan 2025 pada 08.11
--- -- Versi server: 10.4.32-MariaDB
--- -- Versi PHP: 8.1.25
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2025 at 07:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
 
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- START TRANSACTION;
--- SET time_zone = "+00:00";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
--- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
--- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
--- /*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- --
--- -- Database: `db_perpustakaan`
--- --
+--
+-- Database: `db_perpustakaan`
+--
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `fakultas`
--- --
+--
+-- Table structure for table `fakultas`
+--
 
--- CREATE TABLE `fakultas` (
---   `id_fakultas` int(11) NOT NULL,
---   `nama_fakultas` varchar(50) NOT NULL,
---   `id_instansi` int(11) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `fakultas`;
+CREATE TABLE `fakultas` (
+  `id_fakultas` int(11) NOT NULL,
+  `nama_fakultas` varchar(50) NOT NULL,
+  `id_instansi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `instansi`
--- --
+--
+-- Table structure for table `instansi`
+--
 
--- CREATE TABLE `instansi` (
---   `id_instantsi` int(11) NOT NULL,
---   `nama_instansi` varchar(50) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `instansi`;
+CREATE TABLE `instansi` (
+  `id_instantsi` int(11) NOT NULL,
+  `nama_instansi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `kategori`
--- --
+--
+-- Table structure for table `kategori`
+--
 
--- CREATE TABLE `kategori` (
---   `id_kategori` int(11) NOT NULL,
---   `nama_kategori` varchar(50) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `kategori`;
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `login`
--- --
+--
+-- Table structure for table `login`
+--
 
--- CREATE TABLE `login` (
---   `id_login` int(11) NOT NULL,
---   `username` varchar(50) NOT NULL,
---   `password` varchar(20) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --
--- -- Dumping data untuk tabel `login`
--- --
+--
+-- Dumping data for table `login`
+--
 
--- INSERT INTO `login` (`id_login`, `username`, `password`) VALUES
--- (1, 'admin', 'admin123'),
--- (2, 'admin', 'admin');
+UPDATE `login` SET `id_login` = 1,`username` = 'admin',`password` = 'admin123' WHERE `login`.`id_login` = 1;
+UPDATE `login` SET `id_login` = 2,`username` = 'admin',`password` = 'admin' WHERE `login`.`id_login` = 2;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `penelitian`
--- --
+--
+-- Table structure for table `penelitian`
+--
 
--- CREATE TABLE `penelitian` (
---   `id_penelitian` int(11) NOT NULL,
---   `judul` varchar(100) NOT NULL,
---   `nama_penulis` varchar(200) NOT NULL,
---   `tahun` int(4) NOT NULL,
---   `id_fakultas` int(11) DEFAULT NULL,
---   `id_kategori` int(11) NOT NULL,
---   `id_rak` varchar(10) NOT NULL,
---   `id_registrasi` int(11) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `penelitian`;
+CREATE TABLE `penelitian` (
+  `id_penelitian` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `nama_penulis` varchar(200) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `id_fakultas` int(11) DEFAULT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `tgl_masuk` date NOT NULL DEFAULT current_timestamp(),
+  `id_rak` varchar(10) NOT NULL,
+  `petugas` varchar(50) NOT NULL DEFAULT 'Admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Struktur dari tabel `petugas`
--- --
+--
+-- Table structure for table `rak`
+--
 
--- CREATE TABLE `petugas` (
---   `nip` int(11) NOT NULL,
---   `nama_petugas` varchar(50) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `rak`;
+CREATE TABLE `rak` (
+  `id_rak` varchar(10) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+--
+-- Dumping data for table `rak`
+--
 
--- --
--- -- Struktur dari tabel `rak`
--- --
+UPDATE `rak` SET `id_rak` = 'A1',`keterangan` = NULL WHERE `rak`.`id_rak` = 'A1';
 
--- CREATE TABLE `rak` (
---   `id_rak` varchar(10) NOT NULL,
---   `keterangan` varchar(100) DEFAULT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Indexes for dumped tables
+--
 
--- -- --------------------------------------------------------
+--
+-- Indexes for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  ADD PRIMARY KEY (`id_fakultas`),
+  ADD KEY `id_instansi` (`id_instansi`);
 
--- --
--- -- Struktur dari tabel `registrasi`
--- --
+--
+-- Indexes for table `instansi`
+--
+ALTER TABLE `instansi`
+  ADD PRIMARY KEY (`id_instantsi`);
 
--- CREATE TABLE `registrasi` (
---   `id_registrasi` int(11) NOT NULL,
---   `tgl_masuk` date NOT NULL DEFAULT current_timestamp(),
---   `id_petugas` int(11) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
 
--- --
--- -- Indexes for dumped tables
--- --
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
 
--- --
--- -- Indeks untuk tabel `fakultas`
--- --
--- ALTER TABLE `fakultas`
---   ADD PRIMARY KEY (`id_fakultas`),
---   ADD KEY `id_instansi` (`id_instansi`);
+--
+-- Indexes for table `penelitian`
+--
+ALTER TABLE `penelitian`
+  ADD PRIMARY KEY (`id_penelitian`),
+  ADD KEY `id_fakultas` (`id_fakultas`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_rak` (`id_rak`);
+ALTER TABLE `penelitian` ADD FULLTEXT KEY `judul` (`judul`,`nama_penulis`);
+ALTER TABLE `penelitian` ADD FULLTEXT KEY `judul_2` (`judul`,`nama_penulis`);
 
--- --
--- -- Indeks untuk tabel `instansi`
--- --
--- ALTER TABLE `instansi`
---   ADD PRIMARY KEY (`id_instantsi`);
+--
+-- Indexes for table `rak`
+--
+ALTER TABLE `rak`
+  ADD PRIMARY KEY (`id_rak`);
 
--- --
--- -- Indeks untuk tabel `kategori`
--- --
--- ALTER TABLE `kategori`
---   ADD PRIMARY KEY (`id_kategori`);
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- --
--- -- Indeks untuk tabel `login`
--- --
--- ALTER TABLE `login`
---   ADD PRIMARY KEY (`id_login`);
+--
+-- AUTO_INCREMENT for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  MODIFY `id_fakultas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
--- --
--- -- Indeks untuk tabel `penelitian`
--- --
--- ALTER TABLE `penelitian`
---   ADD PRIMARY KEY (`id_penelitian`),
---   ADD KEY `id_fakultas` (`id_fakultas`),
---   ADD KEY `id_kategori` (`id_kategori`),
---   ADD KEY `id_rak` (`id_rak`),
---   ADD KEY `id_registrasi` (`id_registrasi`);
+--
+-- AUTO_INCREMENT for table `instansi`
+--
+ALTER TABLE `instansi`
+  MODIFY `id_instantsi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- --
--- -- Indeks untuk tabel `petugas`
--- --
--- ALTER TABLE `petugas`
---   ADD PRIMARY KEY (`nip`);
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
--- --
--- -- Indeks untuk tabel `rak`
--- --
--- ALTER TABLE `rak`
---   ADD PRIMARY KEY (`id_rak`);
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
--- --
--- -- Indeks untuk tabel `registrasi`
--- --
--- ALTER TABLE `registrasi`
---   ADD PRIMARY KEY (`id_registrasi`),
---   ADD KEY `id_petugas` (`id_petugas`);
+--
+-- AUTO_INCREMENT for table `penelitian`
+--
+ALTER TABLE `penelitian`
+  MODIFY `id_penelitian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- --
--- -- AUTO_INCREMENT untuk tabel yang dibuang
--- --
+--
+-- Constraints for dumped tables
+--
 
--- --
--- -- AUTO_INCREMENT untuk tabel `fakultas`
--- --
--- ALTER TABLE `fakultas`
---   MODIFY `id_fakultas` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  ADD CONSTRAINT `fakultas_ibfk_1` FOREIGN KEY (`id_instansi`) REFERENCES `instansi` (`id_instantsi`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- --
--- -- AUTO_INCREMENT untuk tabel `instansi`
--- --
--- ALTER TABLE `instansi`
---   MODIFY `id_instantsi` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for table `penelitian`
+--
+ALTER TABLE `penelitian`
+  ADD CONSTRAINT `penelitian_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penelitian_ibfk_2` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penelitian_ibfk_3` FOREIGN KEY (`id_rak`) REFERENCES `rak` (`id_rak`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- --
--- -- AUTO_INCREMENT untuk tabel `kategori`
--- --
--- ALTER TABLE `kategori`
---   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
-
--- --
--- -- AUTO_INCREMENT untuk tabel `login`
--- --
--- ALTER TABLE `login`
---   MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
--- --
--- -- AUTO_INCREMENT untuk tabel `penelitian`
--- --
--- ALTER TABLE `penelitian`
---   MODIFY `id_penelitian` int(11) NOT NULL AUTO_INCREMENT;
-
--- --
--- -- AUTO_INCREMENT untuk tabel `petugas`
--- --
--- ALTER TABLE `petugas`
---   MODIFY `nip` int(11) NOT NULL AUTO_INCREMENT;
-
--- --
--- -- AUTO_INCREMENT untuk tabel `registrasi`
--- --
--- ALTER TABLE `registrasi`
---   MODIFY `id_registrasi` int(11) NOT NULL AUTO_INCREMENT;
-
--- --
--- -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
--- --
-
--- --
--- -- Ketidakleluasaan untuk tabel `fakultas`
--- --
--- ALTER TABLE `fakultas`
---   ADD CONSTRAINT `fakultas_ibfk_1` FOREIGN KEY (`id_instansi`) REFERENCES `instansi` (`id_instantsi`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- --
--- -- Ketidakleluasaan untuk tabel `penelitian`
--- --
--- ALTER TABLE `penelitian`
---   ADD CONSTRAINT `penelitian_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
---   ADD CONSTRAINT `penelitian_ibfk_2` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`) ON DELETE CASCADE ON UPDATE CASCADE,
---   ADD CONSTRAINT `penelitian_ibfk_3` FOREIGN KEY (`id_rak`) REFERENCES `rak` (`id_rak`) ON DELETE CASCADE ON UPDATE CASCADE,
---   ADD CONSTRAINT `penelitian_ibfk_4` FOREIGN KEY (`id_registrasi`) REFERENCES `registrasi` (`id_registrasi`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- --
--- -- Ketidakleluasaan untuk tabel `registrasi`
--- --
--- ALTER TABLE `registrasi`
---   ADD CONSTRAINT `registrasi_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `penelitian`
 ADD FULLTEXT(`judul`, `nama_penulis`);
--- COMMIT;
+COMMIT;
 
--- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
