e953ae0d9304b04c6327a5faa7a5c6a02a7f59b9
Seynz <elverandirham212@gmail.com>
2025-01-17 10:33:56 +0800

    Menambahkan pengaturan posisi navbar

--- index.php
+++ index.php
@@ -73,6 +73,23 @@ $categories_result = $conn->query($categories_query);
             flex: 0 0 auto;
             width: 25%;
         }
+    
+        .navbar {
+            display: flex;
+            justify-content: space-between; /* Kiri dan kanan */
+            align-items: center; /* Vertikal sejajar */
+        }
+
+        .navbar-left {
+            display: flex;
+            gap: 10px; /* Jarak antar ikon di kiri */
+        }
+
+        .navbar-right {
+            display: flex;
+            gap: 10px; /* Jarak antar ikon di kanan */
+            margin-left: auto; /* Geser ke kanan */
+        }
     </style>
 </head>
 <body class="sb-nav-fixed">
@@ -82,17 +99,19 @@ $categories_result = $conn->query($categories_query);
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar-->
-        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
+        <div class="navbar-right ms-auto">
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </nav>
     <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
