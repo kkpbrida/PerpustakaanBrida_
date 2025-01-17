bfee1ec9305b51285987093290147d80f09e6f51
NazwahIlmi <nazwahilmi05@gmail.com>
2025-01-17 15:37:27 +0800

    tambah popup ketika berhasil input data

--- function.php
+++ function.php
@@ -1,42 +1,4 @@
 <?php
-// session_start();
-// // membuat koneksi ke database
-// $conn = mysqli_connect("localhost", "root", "", "db_perpustakaan");
-
-// if (!$conn) {
-//     echo "Koneksi database gagal!";
-// }
-
-// // tambah penelitian
-// if(isset($_POST['addpenelitian'])) {
-//     $tanggal_registrasi = $_POST['tanggal_registrasi'];
-//     $instansi = $_POST['instansi'];
-//     $fakultas = $_POST['fakultas'];
-//     $kategori = $_POST['kategori'];
-//     $judul = $_POST['judul'];
-//     $tahun = $_POST['tahun'];
-//     $rak = $_POST['rak'];
-
-//     foreach ($_POST['nama_penulis'] as $penulis) {
-//         $addtotable = mysqli_query($conn, "INSERT INTO penelitian (tanggal_registrasi, nama_penulis, instansi, fakultas, kategori, judul, tahun, rak) 
-//         VALUES ('$tanggal_registrasi', '$penulis', '$instansi', '$fakultas', '$kategori', '$judul', '$tahun', '$rak')");
-//     }
-//     if($addtotable) {
-//         echo "<script>
-//             alert('Data penelitian berhasil ditambahkan!');
-//             window.location.href='index.php';
-//         </script>";
-//     } else {
-//         echo "<script>
-//             alert('Data penelitian gagal ditambahkan!');
-//             window.location.href='form.php';
-//         </script>";
-//     }
-
-// }
-?> 
-
-<?php
 session_start();
 // membuat koneksi ke database
 $conn = mysqli_connect("localhost", "root", "", "db_perpustakaan");
@@ -48,24 +10,49 @@ if (!$conn) {
 // tambah penelitian
 if(isset($_POST['addpenelitian'])) {
     $tgl_masuk = $_POST['tgl_masuk'];
-    // $instansi = $_POST['instansi'];
     $fakultas = $_POST['fakultas'];
     $kategori = $_POST['kategori'];
     $judul = $_POST['judul'];
     $tahun = $_POST['tahun'];
     $rak = $_POST['rak'];
 
+    $success = true;
+
     foreach ($_POST['nama_penulis'] as $nama_penulis) {
         $addtotable = mysqli_query($conn, "INSERT INTO penelitian (tgl_masuk, nama_penulis, id_fakultas, id_kategori, judul, tahun, id_rak) 
         VALUES ('$tgl_masuk', '$nama_penulis', '$fakultas', '$kategori', '$judul', '$tahun', '$rak')");
 
         if (!$addtotable) {
+            $success = false;
             die("Error: " . mysqli_error($conn));
         }
     }
-    echo "<script>
-        alert('Data penelitian berhasil ditambahkan!');
-        window.location.href='index.php';
-    </script>";
+
+    if ($success) {
+        echo "<script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+                successModal.show();
+            });
+        </script>";
+    }
 }
-?> 
+?>
+
+<!-- Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="successModalLabel">Success</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Data penelitian berhasil ditambahkan!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
