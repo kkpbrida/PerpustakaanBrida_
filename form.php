c7e3095209a4431ca0129885ca6f79ffbb63c692
NazwahIlmi <nazwahilmi05@gmail.com>
2025-01-17 15:09:28 +0800

    menambahkan icon dropbox di form tambah penelitian

--- form.php
+++ form.php
@@ -15,6 +15,22 @@ require 'cek.php';
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <style>
+            .select-container {
+                position: relative;
+            }
+            .select-container select {
+                width: 100%;
+                padding-right: 30px; /* Adjust this value based on the icon size */
+            }
+            .select-container .fa-caret-down {
+                position: absolute;
+                right: 10px;
+                top: 50%;
+                transform: translateY(-50%);
+                pointer-events: none;
+            }
+        </style>
     </head>
     <body class="sb-nav-fixed">
         <!-- Navbar -->
@@ -123,111 +139,104 @@ require 'cek.php';
                                     </script>
                                     <div class="mb-3">
                                         <label for="instansi" class="form-label">Instansi:</label>
-                                        <select id="instansi" name="instansi" class="form-control" required>
-                                            <?php
-                                                $getdata = mysqli_query($conn, "SELECT * FROM instansi");
-                                                if (!$getdata) {
-                                                    die("Error fetching data: " . mysqli_error($conn));
-                                                }
-                                                while ($fetcharray = mysqli_fetch_array($getdata)) {
-                                                    $nama_instansi = $fetcharray['nama_instansi'];
-                                                    $id_instansi = $fetcharray['id_instansi'];
-                                            ?>
-                                            <option value="<?php echo $id_instansi; ?>"><?php echo $nama_instansi; ?></option>
-                                            <?php
-                                                }
-                                            ?>
-                                        </select>
+                                        <div class="select-container">
+                                            <select id="instansi" name="instansi" class="form-control" required>
+                                                <option value="" disabled selected>Pilih Instansi</option>
+                                                <?php
+                                                    $getdata = mysqli_query($conn, "SELECT * FROM instansi");
+                                                    if (!$getdata) {
+                                                        die("Error fetching data: " . mysqli_error($conn));
+                                                    }
+                                                    while ($fetcharray = mysqli_fetch_array($getdata)) {
+                                                        $nama_instansi = $fetcharray['nama_instansi'];
+                                                        $id_instansi = $fetcharray['id_instansi'];
+                                                ?>
+                                                <option value="<?php echo $id_instansi; ?>"><?php echo $nama_instansi; ?></option>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </select>
+                                            <i class="fa fa-caret-down"></i>
+                                        </div>
                                     </div>
                                     <div class="mb-3">
                                         <label for="fakultas" class="form-label">Fakultas:</label>
-                                        <select id="fakultas" name="fakultas" class="form-control" required>
-                                            <?php
-                                                $getdata = mysqli_query($conn, "SELECT * FROM fakultas");
-                                                if (!$getdata) {
-                                                    die("Error fetching data: " . mysqli_error($conn));
-                                                }
-                                                while ($fetcharray = mysqli_fetch_array($getdata)) {
-                                                    $nama_fakultas = $fetcharray['nama_fakultas'];
-                                                    $id_fakultas = $fetcharray['id_fakultas'];
-                                            ?>
-                                            <option value="<?php echo $id_fakultas; ?>"><?php echo $nama_fakultas; ?></option>
-                                            <?php
-                                                }
-                                            ?>
-                                        </select>
-                                    </div>
-                                    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
-                                    <!-- <script>
-                                        $(document).ready(function() {
-                                            $('#instansi').on('change', function() {
-                                                var id_instansi = $(this).val(); // Ambil nilai instansi yang dipilih
-
-                                                // Kosongkan dropdown fakultas jika instansi tidak dipilih
-                                                if (id_instansi === "") {
-                                                    $('#fakultas').html('<option value="">-- Pilih Fakultas --</option>');
-                                                    return;
-                                                }
-
-                                                // Mengambil data fakultas berdasarkan instansi
-                                                $.ajax({
-                                                    url: 'form.php', // URL file PHP untuk mengambil data fakultas
-                                                    type: 'POST',
-                                                    data: { id_instansi: id_instansi },
-                                                    success: function(data) {
-                                                        $('#fakultas').html(data); // Isi dropdown fakultas dengan data dari server
-                                                    },
-                                                    error: function(xhr, status, error) {
-                                                        alert('Terjadi kesalahan: ' + error);
+                                        <div class="select-container">
+                                            <select id="fakultas" name="fakultas" class="form-control" required>
+                                                <option value="" disabled selected>Pilih Fakultas</option>
+                                                <?php
+                                                    $getdata = mysqli_query($conn, "SELECT * FROM fakultas");
+                                                    if (!$getdata) {
+                                                        die("Error fetching data: " . mysqli_error($conn));
                                                     }
-                                                });
-                                            });
-                                        });
-                                    </script> -->
+                                                    while ($fetcharray = mysqli_fetch_array($getdata)) {
+                                                        $nama_fakultas = $fetcharray['nama_fakultas'];
+                                                        $id_fakultas = $fetcharray['id_fakultas'];
+                                                ?>
+                                                <option value="<?php echo $id_fakultas; ?>"><?php echo $nama_fakultas; ?></option>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </select>
+                                            <i class="fa fa-caret-down"></i>
+                                        </div>
+                                    </div>
                                     <div class="mb-3">
                                         <label for="kategori" class="form-label">Kategori:</label>
-                                        <select id="kategori" name="kategori" class="form-control" required>
-                                            <?php
-                                                $getdata = mysqli_query($conn, "SELECT * FROM kategori");
-                                                if (!$getdata) {
-                                                    die("Error fetching data: " . mysqli_error($conn));
-                                                }
-                                                while ($fetcharray = mysqli_fetch_array($getdata)) {
-                                                    $nama_kategori = $fetcharray['nama_kategori'];
-                                                    $id_kategori = $fetcharray['id_kategori'];
-                                            ?>
-                                            <option value="<?php echo $id_kategori; ?>"><?php echo $nama_kategori; ?></option>
-                                            <?php
-                                                }
-                                            ?>
-                                        </select>
+                                        <div class="select-container">
+                                            <select id="kategori" name="kategori" class="form-control" required>
+                                                <option value="" disabled selected>Pilih Kategori</option>
+                                                <?php
+                                                    $getdata = mysqli_query($conn, "SELECT * FROM kategori");
+                                                    if (!$getdata) {
+                                                        die("Error fetching data: " . mysqli_error($conn));
+                                                    }
+                                                    while ($fetcharray = mysqli_fetch_array($getdata)) {
+                                                        $nama_kategori = $fetcharray['nama_kategori'];
+                                                        $id_kategori = $fetcharray['id_kategori'];
+                                                ?>
+                                                <option value="<?php echo $id_kategori; ?>"><?php echo $nama_kategori; ?></option>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </select>
+                                            <i class="fa fa-caret-down"></i>
+                                        </div>
                                     </div>
                                     <div class="mb-3">
                                         <label for="tahun" class="form-label">Tahun:</label>
-                                        <select id="tahun" name="tahun" class="form-control" required>
-                                            <?php
-                                                for ($year = 2019; $year <= 2029; $year++) {
-                                                    echo "<option value='$year'>$year</option>";
-                                                }
-                                            ?>
-                                        </select>
+                                        <div class="select-container">
+                                            <select id="tahun" name="tahun" class="form-control" required>
+                                                <option value="" disabled selected>Pilih Tahun</option>
+                                                <?php
+                                                    for ($year = 2019; $year <= 2029; $year++) {
+                                                        echo "<option value='$year'>$year</option>";
+                                                    }
+                                                ?>
+                                            </select>
+                                            <i class="fa fa-caret-down"></i>
+                                        </div>
                                     </div>
                                     <div class="mb-3">
                                         <label for="rak" class="form-label">Rak:</label>
-                                        <select id="rak" name="rak" class="form-control" required>
-                                            <?php
-                                                $getdata = mysqli_query($conn, "SELECT id_rak FROM rak");
-                                                if (!$getdata) {
-                                                    die("Error fetching data: " . mysqli_error($conn));
-                                                }
-                                                while ($fetcharray = mysqli_fetch_array($getdata)) {
-                                                    $id_rak = $fetcharray['id_rak'];
-                                            ?>
-                                            <option value="<?php echo $id_rak; ?>"><?php echo $id_rak; ?></option>
-                                            <?php
-                                                }
-                                            ?>
-                                        </select>
+                                        <div class="select-container">
+                                            <select id="rak" name="rak" class="form-control" required>
+                                                <option value="" disabled selected>Pilih Rak</option>
+                                                <?php
+                                                    $getdata = mysqli_query($conn, "SELECT id_rak FROM rak");
+                                                    if (!$getdata) {
+                                                        die("Error fetching data: " . mysqli_error($conn));
+                                                    }
+                                                    while ($fetcharray = mysqli_fetch_array($getdata)) {
+                                                        $id_rak = $fetcharray['id_rak'];
+                                                ?>
+                                                <option value="<?php echo $id_rak; ?>"><?php echo $id_rak; ?></option>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </select>
+                                            <i class="fa fa-caret-down"></i>
+                                        </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary" name="addpenelitian">Submit</button>
                                 </form>
