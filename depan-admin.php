0b4044c4db9019496ca93fc9bf2027a919a29eec
Muhammad Dimas R. <draxozy@gmail.com>
2025-01-22 12:09:15 +0800

    Update depan-admin.php

--- depan-admin.php
+++ depan-admin.php
@@ -401,7 +401,7 @@ $rak_result = mysqli_query($conn, "SELECT * FROM rak");
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; KKP ILMU KOMPUTER</div>
+            <div class="text-muted"> &copy;KKP Ilmu Komputer UHO 2025</div>
         </div>
     </div>
 </footer>
